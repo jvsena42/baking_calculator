e24ae74df51cc82aa9076d45f223e49fcffc69ce
jvsena42 <jvsena16@gmail.com>
2022-05-01 19:42:57 -0300

    feat: firestore

---
+++ app/src/main/java/com/bulletapps/candypricer/data/FirebaseSupplyDataSource.kt
@@ -0,0 +1,9 @@
+package com.bulletapps.candypricer.data
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirebaseSupplyDataSource(
+    firebaseFirestore: FirebaseFirestore,
+
+    ) {
+}
--- app/src/main/java/com/bulletapps/candypricer/data/SupplyDataSource.kt
+++ app/src/main/java/com/bulletapps/candypricer/data/SupplyDataSource.kt
@@ -3,9 +3,9 @@ package com.bulletapps.candypricer.data
 import com.bulletapps.candypricer.domain.model.Supply
 
 interface SupplyDataSource {
-    fun createSupply(supply: Supply)
+    fun createSupply(supply: Supply): Supply
 
-    fun getSupply(id: String) : Supply
+    fun getSupply(id: String): Supply
 
     fun getAllSupplies() : List<Supply>
 
