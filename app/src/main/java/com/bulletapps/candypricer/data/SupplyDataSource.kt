8978d7cf1c8dd7da792a5927a81128e7a08da15b
jvsena42 <jvsena16@gmail.com>
2022-05-01 18:16:52 -0300

    feat: SupplyDataSource.kt

---
+++ app/src/main/java/com/bulletapps/candypricer/data/SupplyDataSource.kt
@@ -0,0 +1,15 @@
+package com.bulletapps.candypricer.data
+
+import com.bulletapps.candypricer.domain.model.Supply
+
+interface SupplyDataSource {
+    fun createSupply(supply: Supply)
+
+    fun getSupply(id: String) : Supply
+
+    fun getAllSupplies() : List<Supply>
+
+    fun updateSupply(id: String)
+
+    fun deleteSupply(id: String)
+}
--- app/src/main/java/com/bulletapps/candypricer/domain/model/Supply.kt
+++ app/src/main/java/com/bulletapps/candypricer/domain/model/Supply.kt
@@ -1,6 +1,6 @@
 package com.bulletapps.candypricer.domain.model
 
-data class Supplies(
+data class Supply(
     val id: String = "",
     val description: String = "",
     val price: Double = 0.0,
