02712ba62759500a85d9208ad99b98e3cbf19423
jvsena42 <jvsena16@gmail.com>
2022-05-29 17:07:55 -0300

    feat: SupplyLocalDataSourceModule.kt

---
+++ app/src/main/java/com/bulletapps/candypricer/data/datasource/di/SupplyLocalDataSourceModule.kt
@@ -0,0 +1,21 @@
+package com.bulletapps.candypricer.data.datasource.di
+
+import com.bulletapps.candypricer.data.datasource.SupplyDataSource
+import com.bulletapps.candypricer.data.datasource.SupplyLocalDataSourceImpl
+import com.bulletapps.candypricer.data.db.SupplyDAO
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class SupplyLocalDataSourceModule {
+
+    @Singleton
+    @Provides
+    fun providesSupplyLocalDataSourceModule(supplyDAO: SupplyDAO): SupplyDataSource {
+        return SupplyLocalDataSourceImpl(supplyDAO)
+    }
+}
