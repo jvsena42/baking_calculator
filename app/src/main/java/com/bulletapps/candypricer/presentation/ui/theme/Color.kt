65a06b49a89daa5ab069853460391745578ef408
jvsena42 <jvsena16@gmail.com>
2022-05-29 19:38:44 -0300

    feat: colors

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.bulletapps.candypricer.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val primary = Color(0xFFF44336)
+val primaryDark = Color(0xFFD32F2F)
+val primaryLight = Color(0xFFFFCDD2)
+
+val colorAccent = Color(0xFFFFC107)
+val primaryText = Color(0xFF212121)
+val secondaryText = Color(0xFF757575)
+
+val colorDivider = Color(0xFFBDBDBD)
