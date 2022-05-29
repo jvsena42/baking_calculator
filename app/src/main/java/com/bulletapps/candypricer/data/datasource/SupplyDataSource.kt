44539f3d06d9756c1795a374b6ae2177d5bfaa92
jvsena42 <jvsena16@gmail.com>
2022-05-29 16:50:13 -0300

    feat: implement SupplyLocalDataSourceImpl.kt

--- app/src/main/java/com/bulletapps/candypricer/data/datasource/SupplyDataSource.kt
+++ app/src/main/java/com/bulletapps/candypricer/data/datasource/SupplyDataSource.kt
@@ -3,13 +3,13 @@ package com.bulletapps.candypricer.data.datasource
 import com.bulletapps.candypricer.data.model.Supply
 
 interface SupplyDataSource {
-    suspend fun createSupply(supply: Supply): Supply
+    suspend fun createSupply(supply: Supply)
 
     suspend fun getSupply(id: String): Supply?
 
     suspend fun getAllSupplies() : List<Supply>
 
-    suspend fun updateSupply(supply: Supply) : Supply
+    suspend fun updateSupply(supply: Supply)
 
-    suspend fun deleteSupply(id: String)
+    suspend fun deleteSupply(supply: Supply)
 }
--- app/src/main/java/com/bulletapps/candypricer/data/datasource/SupplyLocalDataSourceImpl.kt
+++ app/src/main/java/com/bulletapps/candypricer/data/datasource/SupplyLocalDataSourceImpl.kt
@@ -1,25 +1,17 @@
 package com.bulletapps.candypricer.data.datasource
 
+import com.bulletapps.candypricer.data.db.SupplyDAO
 import com.bulletapps.candypricer.data.model.Supply
+import javax.inject.Inject
 
-class SupplyLocalDataSourceImpl constructor( ): SupplyDataSource {
-    override suspend fun createSupply(supply: Supply): Supply {
-        TODO("Not yet implemented")
-    }
+class SupplyLocalDataSourceImpl @Inject constructor(private val supplyDAO: SupplyDAO ): SupplyDataSource {
+    override suspend fun createSupply(supply: Supply) = supplyDAO.createSupply(supply)
 
-    override suspend fun getSupply(id: String): Supply? {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getSupply(id: String) = supplyDAO.getSupply(id)
 
-    override suspend fun getAllSupplies(): List<Supply> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getAllSupplies() = supplyDAO.getAllSupplies()
 
-    override suspend fun updateSupply(supply: Supply): Supply {
-        TODO("Not yet implemented")
-    }
+    override suspend fun updateSupply(supply: Supply) = supplyDAO.updateSupply(supply)
 
-    override suspend fun deleteSupply(id: String) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteSupply(supply: Supply) = supplyDAO.deleteSupply(supply)
 }
