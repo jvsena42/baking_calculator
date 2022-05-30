fa72a6322975d121fed022d1aa364fc2ab6d42cf
jvsena42 <jvsena16@gmail.com>
2022-05-30 07:54:18 -0300

    feat: top app bar

--- app/src/main/java/com/bulletapps/candypricer/MainActivity.kt
+++ app/src/main/java/com/bulletapps/candypricer/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.bulletapps.candypricer.presentation.ui.theme.CandyPricerTheme
 
@@ -16,28 +17,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CandyPricerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            BuildUI()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BuildUI() {
+    CandyPricerTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(1f),
+                            textAlign = TextAlign.Center,
+                            text = stringResource(R.string.main_menu)
+                        )
+                    },
+                )
+            }
+        ) {
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CandyPricerTheme {
-        Greeting("Android")
-    }
+    BuildUI()
 }
