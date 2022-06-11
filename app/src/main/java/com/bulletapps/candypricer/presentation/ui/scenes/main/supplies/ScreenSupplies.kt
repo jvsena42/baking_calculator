1aae7468bca91712cbba35327f4c86f84c14b6f9
jvsena42 <jvsena16@gmail.com>
2022-06-11 06:01:08 -0300

    feat: app bar with title (wip)

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/supplies/ScreenSupplies.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/supplies/ScreenSupplies.kt
@@ -3,11 +3,14 @@
 package com.bulletapps.candypricer.presentation.ui.scenes.main.supplies
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -15,6 +18,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -43,14 +48,20 @@ fun Screen(
                 .background(color=colors.background),
         ) {
             TopAppBar(
-                title = {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        text = stringResource(R.string.supplies)
+                backgroundColor = colors.primary,
+            ) {
+                IconButton(onClick = {  }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_arrow_back),
+                        contentDescription = stringResource(R.string.back),
                     )
-                },
-            )
+                }
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = stringResource(R.string.supplies)
+                )
+            }
             SuppliesList(items.value)
         }
     }
