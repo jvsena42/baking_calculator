e2f2a99e1921911d2706c0ba2bff973e6c0df163
jvsena42 <jvsena16@gmail.com>
2022-06-13 07:32:44 -0300

    grat: dropdown wip

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/addSupply/SreenAddSupply.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/addSupply/SreenAddSupply.kt
@@ -1,11 +1,9 @@
 package com.bulletapps.candypricer.presentation.ui.scenes.main.addSupply
 
+import android.widget.Space
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -13,7 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.bulletapps.candypricer.R
 import com.bulletapps.candypricer.presentation.ui.scenes.main.MainViewModel
@@ -47,7 +47,7 @@ fun Screen(
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = MaterialTheme.colors.background),
-            verticalArrangement = CustomArrangement.spacedBy(8.dp)
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             TopAppBar(
                 title = {
@@ -59,7 +59,7 @@ fun Screen(
                 },
             )
 
-            Spacer(Modifier.padding(8.dp))
+            Spacer(Modifier.height(8.dp))
 
             OutlinedTextField(
                 value = name,
@@ -74,8 +74,30 @@ fun Screen(
                 onValueChange = { onTextChanged(FieldsTexts.Quantity(it)) },
                 placeholder = { Text(stringResource(R.string.five_hundred)) },
                 label = { Text(stringResource(R.string.quantity)) },
-                modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth()
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
             )
+
+            DropdownMenu(
+                expanded = true,
+                onDismissRequest = {},
+                offset = DpOffset(0.dp, 0.dp),
+                properties = PopupProperties(focusable = true),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
+            ) {
+                DropdownMenuItem(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
+                ) {
+                    Text("gramas")
+                }
+
+                DropdownMenuItem(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
+                ) {
+                    Text("gramas")
+                }
+            }
 
             OutlinedTextField(
                 value = price,
@@ -84,6 +106,8 @@ fun Screen(
                 label = { Text(stringResource(R.string.price)) },
                 modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth()
             )
+
+            Spacer(modifier = Modifier.weight(1f))
 
             NormalButton(text = stringResource(R.string.confirm), onClick = onClick)
         }
