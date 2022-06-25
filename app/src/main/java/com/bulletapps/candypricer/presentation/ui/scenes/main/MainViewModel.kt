524ec913d0824cdbfc7d51c0c51c612a7c25b050
João Victor Sena <jvsena16@gmail.com>
2022-06-25 08:53:56 -0300

    Feat/login UI (#17)
    * feat: setup ui wip
    
    * feat: MakeRegisterText
    
    * feat: icon

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/MainViewModel.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/MainViewModel.kt
@@ -32,6 +32,7 @@ class MainViewModel @Inject constructor(): ViewModel(), EventFlow<MainViewModel.
         object Supplies : Navigation("supplies")
         object AddSupply : Navigation("add_supply")
         object Settings : Navigation("settings")
+        object Login : Navigation("login")
     }
 }
 
---
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/login/ScreenLogin.kt
@@ -0,0 +1,122 @@
+package com.bulletapps.candypricer.presentation.ui.scenes.main.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.bulletapps.candypricer.R
+import com.bulletapps.candypricer.presentation.ui.scenes.main.MainViewModel
+import com.bulletapps.candypricer.presentation.ui.scenes.main.login.LoginViewModel.*
+import com.bulletapps.candypricer.presentation.ui.scenes.main.login.LoginViewModel.ScreenActions.OnClickConfirm
+import com.bulletapps.candypricer.presentation.ui.scenes.main.login.LoginViewModel.ScreenActions.OnTextChanged
+import com.bulletapps.candypricer.presentation.ui.theme.CandyPricerTheme
+import com.bulletapps.candypricer.presentation.ui.widgets.NormalButton
+
+@Composable
+fun ScreenLogin(
+    viewModel: LoginViewModel = hiltViewModel(),
+    sharedViewModel: MainViewModel
+) {
+    Screen(viewModel.uiState, viewModel::onAction)
+}
+
+
+@Composable
+private fun Screen(
+    uiState: UIState,
+    onAction: (ScreenActions) -> Unit,
+) {
+    CandyPricerTheme {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colors.background),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(Modifier.height(32.dp))
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_logo_yellow),
+                contentDescription = stringResource(id = R.string.app_name),
+                modifier = Modifier.padding(16.dp)
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            MakeFieldEmail(onAction, uiState)
+
+            Spacer(Modifier.height(4.dp))
+
+            MakeFieldPassword(onAction, uiState)
+
+            NormalButton(
+                text = stringResource(R.string.login),
+                onClick = { onAction(OnClickConfirm) }
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            MakeRegisterText(onAction)
+
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
+}
+
+@Composable
+private fun MakeRegisterText(onAction: (ScreenActions) -> Unit) {
+    Column() {
+        Text(stringResource(R.string.dont_you_have_an_account))
+        TextButton(onClick = { onAction(ScreenActions.OnClickRegister) }) {
+            Text(stringResource(R.string.do_sign_in))
+        }
+    }
+}
+
+
+@Composable
+private fun MakeFieldEmail(onAction: (ScreenActions) -> Unit, uiState: UIState) {
+    val name by uiState.email.collectAsState()
+
+    OutlinedTextField(
+        value = name,
+        singleLine = true,
+        onValueChange = { onAction(OnTextChanged(FieldsTexts.Email(it))) },
+        label = { Text(stringResource(R.string.email)) },
+        modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth()
+    )
+}
+
+@Composable
+private fun MakeFieldPassword(onAction: (ScreenActions) -> Unit, uiState: UIState) {
+    val password by uiState.password.collectAsState()
+
+    OutlinedTextField(
+        value = password,
+        singleLine = true,
+        onValueChange = { onAction(OnTextChanged(FieldsTexts.Password(it))) },
+        label = { Text(stringResource(R.string.password)) },
+        modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth()
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    Screen(
+        onAction = {},
+        uiState = UIState()
+    )
+}
