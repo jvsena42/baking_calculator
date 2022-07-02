d625701ae9bd206e7c778a3a5d0ba61246b7c5ad
jvsena42 <jvsena16@gmail.com>
2022-07-02 11:45:57 -0300

    feat: email text validation

--- app/src/main/java/com/bulletapps/candypricer/domain/usecase/SubmitEmailUseCaseImpl.kt
+++ app/src/main/java/com/bulletapps/candypricer/domain/usecase/SubmitEmailUseCaseImpl.kt
@@ -5,13 +5,13 @@ import com.bulletapps.candypricer.config.Resource
 import com.bulletapps.candypricer.config.UiText
 import javax.inject.Inject
 
-class SubmitEmailUseCaseImpl @Inject constructor(): SubmitEmailUseCase {
+class SubmitEmailUseCaseImpl @Inject constructor() : SubmitEmailUseCase {
     override suspend fun invoke(email: String): Resource<Unit> {
-        if(!email.contains("@")) {
-            return Resource.Error(
-                UiText.StringResource(R.string.error_invalid_email)
-            )
+        if (email.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return Resource.Success(null)
         }
-        return Resource.Success(null)
+        return Resource.Error(
+            UiText.StringResource(R.string.error_invalid_email)
+        )
     }
 }
