fa12b71d9574f50b33f169cef33b047462f0abd4
jvsena42 <jvsena16@gmail.com>
2022-07-06 07:16:00 -0300

    feat: api interface wip

--- app/src/main/java/com/bulletapps/candypricer/data/api/CandyPricerApi.kt
+++ app/src/main/java/com/bulletapps/candypricer/data/api/CandyPricerApi.kt
@@ -1,12 +1,23 @@
 package com.bulletapps.candypricer.data.api
 
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface CandyPricerApi {
-    @GET("v2/top-headlines")
-    suspend fun getSupplies(
-        @Query("sources") sources: String,
-        @Query("page") page: Int,
+    @GET("/unit")
+    suspend fun getUnits(
+        @Query("key") key: String,
+        @Query("value") value: String,
+        @Query("disabled") disabled: Boolean,
     )
+
+    @POST("/supply")
+    suspend fun createSupply()
+
+    @POST("/user")
+    suspend fun createUser()
+
+    @POST("/login")
+    suspend fun login()
 }
