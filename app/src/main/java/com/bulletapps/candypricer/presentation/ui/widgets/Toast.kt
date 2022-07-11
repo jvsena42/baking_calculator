ebb0bda9887b3a8e3f7803705eedf44988242ba0
jvsena42 <jvsena16@gmail.com>
2022-07-11 07:09:23 -0300

    feat: Toast.kt

---
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/widgets/Toast.kt
@@ -0,0 +1,15 @@
+package com.bulletapps.candypricer.presentation.ui.widgets
+
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun Toast(
+    text: String,
+    duration: Int = Toast.LENGTH_LONG
+) {
+    val context = LocalContext.current
+    val toast = Toast.makeText(context, text, duration)
+    toast.show()
+}
