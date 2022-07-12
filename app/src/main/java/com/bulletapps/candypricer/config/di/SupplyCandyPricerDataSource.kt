a920aa7ae13119bc31fecc3647f4f0525458f7ef
jvsena42 <jvsena16@gmail.com>
2022-07-12 07:03:22 -0300

    feat: providePreferencesDataStore

--- app/src/main/java/com/bulletapps/candypricer/config/di/SupplyCandyPricerDataSource.kt
+++ app/src/main/java/com/bulletapps/candypricer/config/di/SupplyCandyPricerDataSource.kt
@@ -1,5 +1,10 @@
 package com.bulletapps.candypricer.config.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.bulletapps.candypricer.data.api.CandyPricerApi
 import com.bulletapps.candypricer.data.datasource.CandyPricerDataSource
 import com.bulletapps.candypricer.data.datasource.CandyPricerRemoteDataSource
@@ -8,8 +13,11 @@ import com.bulletapps.candypricer.data.repository.CandyPricerRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
+private const val PREFERENCES_STORE_NAME = "settings"
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,7 +25,16 @@ class SupplyCandyPricerDataSource {
 
     @Singleton
     @Provides
-    fun providesCandyPricerRemoteDataSource (api: CandyPricerApi): CandyPricerDataSource {
+    fun providesCandyPricerRemoteDataSource(api: CandyPricerApi): CandyPricerDataSource {
         return CandyPricerRemoteDataSource(api)
     }
+
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(@ApplicationContext appContext: Context): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            produceFile = {
+                appContext.preferencesDataStoreFile(PREFERENCES_STORE_NAME)
+            }
+        )
 }
