c18af31f121be84b15222d99b4e11f1d34155c29
jvsena42 <jvsena16@gmail.com>
2022-07-17 11:22:13 -0300

    feat: setup

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/user/supplies/SuppliesViewModel.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/user/supplies/SuppliesViewModel.kt
@@ -1,8 +1,13 @@
 package com.bulletapps.candypricer.presentation.ui.scenes.main.user.supplies
 
 import androidx.lifecycle.ViewModel
+import com.bulletapps.candypricer.config.Resource
+import com.bulletapps.candypricer.config.UiText
+import com.bulletapps.candypricer.data.response.SupplyResponse
+import com.bulletapps.candypricer.data.response.UnitResponse
 import com.bulletapps.candypricer.domain.model.Supply
 import com.bulletapps.candypricer.domain.usecase.supply.GetAllSuppliesUseCase
+import com.bulletapps.candypricer.presentation.ui.scenes.main.user.addSupply.AddSupplyViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
@@ -12,12 +17,28 @@ class SuppliesViewModel @Inject constructor(
     private val getAllSuppliesUseCase: GetAllSuppliesUseCase
 ) : ViewModel() {
 
-    val suppliesList: MutableStateFlow<List<Supply>> = MutableStateFlow(
-        listOf(
-            Supply(id = 0, name = "Leite Condensado Caixa", price = "R$ 5,00", quantity = 1.0, unitType = "Unidade" ),
-            Supply(id = 1, name = "Creme de leite Caixa", price = "R$ 6,00", quantity = 1.0, unitType = "Unidade" ),
-            Supply(id = 2, name = "Chocolate em pó", price = "R$ 38,00", quantity = 500.0, unitType = "Gramas" ),
-        ),
-    )
+    val uiState = UIState()
+
+    suspend fun setup() {
+        val suppliesResult = getAllSuppliesUseCase()
+        when(suppliesResult) {
+            is Resource.Error -> showToast(suppliesResult.message)
+            is Resource.Success -> uiState.suppliesList.value = suppliesResult.data!!
+        }
+    }
+
+
+    private fun showToast(message: UiText?) {
+        message?.let{ uiState.textToast.value = it }
+    }
+
+    sealed class ScreenActions {
+        object OnClickAdd : ScreenActions()
+    }
+
+    class UIState {
+        val suppliesList = MutableStateFlow<List<SupplyResponse>>(emptyList())
+        val textToast = MutableStateFlow<UiText>(UiText.DynamicString(""))
+    }
 }
 
