fb859048b59186fd64279176ad08a93072c37f51
João Victor Sena <jvsena16@gmail.com>
2022-07-21 06:57:29 -0300

    feat: logout (#55)

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/user/settings/SettingsViewModel.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/user/settings/SettingsViewModel.kt
@@ -2,16 +2,21 @@ package com.bulletapps.candypricer.presentation.ui.scenes.main.user.settings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bulletapps.candypricer.data.datasource.PreferencesDataSource
 import com.bulletapps.candypricer.presentation.util.EventFlow
 import com.bulletapps.candypricer.presentation.util.EventFlowImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor() : ViewModel(), EventFlow<SettingsViewModel.ScreenEvent> by EventFlowImpl() {
+class SettingsViewModel @Inject constructor(
+    private val preferencesDataSource: PreferencesDataSource
+) : ViewModel(), EventFlow<SettingsViewModel.ScreenEvent> by EventFlowImpl() {
 
-    private fun onClickConfirm() {
-        viewModelScope.sendEvent(ScreenEvent.Login)
+    private fun onClickConfirm() = viewModelScope.launch {
+        preferencesDataSource.clearPref()
+        sendEvent(ScreenEvent.Login)
     }
 
     fun onAction(action: ScreenActions) = when(action) {
