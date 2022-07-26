bd0f6102f3e6efe8be96bc48c071987b2e9a716d
João Victor Sena <jvsena16@gmail.com>
2022-07-26 07:43:56 -0300

    feat: product detail ui (#60)

---
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/user/productDetail/ProductDetailViewModel.kt
@@ -0,0 +1,54 @@
+package com.bulletapps.candypricer.presentation.ui.scenes.main.user.productDetail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bulletapps.candypricer.config.Resource
+import com.bulletapps.candypricer.config.UiText
+import com.bulletapps.candypricer.data.parameters.CreateProductParameters
+import com.bulletapps.candypricer.data.response.SupplyResponse
+import com.bulletapps.candypricer.data.response.UnitResponse
+import com.bulletapps.candypricer.domain.usecase.inputValidation.ValidateEmptyListUseCase
+import com.bulletapps.candypricer.domain.usecase.inputValidation.ValidateEmptyTextUseCase
+import com.bulletapps.candypricer.domain.usecase.product.CreateProductUseCase
+import com.bulletapps.candypricer.domain.usecase.supply.GetAllSuppliesUseCase
+import com.bulletapps.candypricer.domain.usecase.unit.GetUnitsUseCase
+import com.bulletapps.candypricer.presentation.ui.scenes.main.user.addProduct.AddProductViewModel.*
+import com.bulletapps.candypricer.presentation.util.EventFlow
+import com.bulletapps.candypricer.presentation.util.EventFlowImpl
+import com.bulletapps.candypricer.presentation.util.orZero
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductDetailViewModel @Inject constructor( ) : ViewModel(), EventFlow<ProductDetailViewModel.ScreenEvent> by EventFlowImpl() {
+
+    val uiState = UIState()
+    private val emptySupply = SupplyResponse(id = -1, name = "", quantity = 0, value = 0.0, null)
+
+    fun onAction(action: ScreenActions) = when(action) {
+        is ScreenActions.OnClickDelete -> {}
+        is ScreenActions.OnClickEdit -> {}
+    }
+
+    sealed class ScreenEvent {
+        object GoBack : ScreenEvent()
+    }
+
+    sealed class ScreenActions {
+        object OnClickDelete : ScreenActions()
+        object OnClickEdit : ScreenActions()
+    }
+
+    class UIState {
+        val selectedSupplies = MutableStateFlow(mutableListOf<MenuItemModel>())
+    }
+
+    data class MenuItemModel(
+        val id: Int,
+        val name: String,
+        val qut: Int
+    )
+}
+
