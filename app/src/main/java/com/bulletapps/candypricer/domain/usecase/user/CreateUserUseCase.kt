3d24708654cde140d16d588f6fa6a543f4d1facb
jvsena42 <jvsena16@gmail.com>
2022-07-31 14:22:26 -0300

    fix: clean phone

--- app/src/main/java/com/bulletapps/candypricer/domain/usecase/user/CreateUserUseCase.kt
+++ app/src/main/java/com/bulletapps/candypricer/domain/usecase/user/CreateUserUseCase.kt
@@ -1,9 +1,15 @@
 package com.bulletapps.candypricer.domain.usecase.user
 
+import com.bulletapps.candypricer.config.Resource
 import com.bulletapps.candypricer.data.parameters.CreateUserParameters
 import com.bulletapps.candypricer.data.repository.CandyPricerRepository
+import com.bulletapps.candypricer.data.response.LoginResponse
+import com.bulletapps.candypricer.presentation.util.filterNumbers
 import javax.inject.Inject
 
 class CreateUserUseCase @Inject constructor(private val repository: CandyPricerRepository) {
-    suspend operator fun invoke(parameters: CreateUserParameters) = repository.createUser(parameters)
+    suspend operator fun invoke(parameters: CreateUserParameters): Resource<LoginResponse> {
+        val cleanPhone = parameters.copy(email = parameters.phone.filterNumbers())
+        return repository.createUser(cleanPhone)
+    }
 }
