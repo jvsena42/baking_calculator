2d5e9437352500c4d3b5e55a37e66759b4def482
jvsena42 <jvsena16@gmail.com>
2022-08-08 07:14:14 -0300

    feat: handle menu clients

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/MainViewModel.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/MainViewModel.kt
@@ -3,9 +3,13 @@ package com.bulletapps.candypricer.presentation.ui.scenes.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bulletapps.candypricer.R
+import com.bulletapps.candypricer.config.Resource
 import com.bulletapps.candypricer.domain.model.MenuModel
+import com.bulletapps.candypricer.domain.usecase.user.GetUserUseCase
+import com.bulletapps.candypricer.presentation.ui.scenes.main.menu.MenuViewModel
 import com.bulletapps.candypricer.presentation.util.EventFlow
 import com.bulletapps.candypricer.presentation.util.EventFlowImpl
+import com.bulletapps.candypricer.presentation.util.orFalse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,21 +17,37 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(): ViewModel(), EventFlow<MainViewModel.Navigation> by EventFlowImpl()  {
+class MainViewModel @Inject constructor(
+    private val getUserUseCase: GetUserUseCase,
+    ): ViewModel(), EventFlow<MainViewModel.Navigation> by EventFlowImpl()  {
 
     val isLoading = MutableStateFlow(true)
 
-    val menuItems: MutableStateFlow<List<MenuModel>> = MutableStateFlow(
-        listOf(
-            MenuModel(R.string.my_products, R.drawable.ic_store, Navigation.Products),
-            MenuModel(R.string.supplies, R.drawable.ic_shopping_cart, Navigation.Supplies),
-//            MenuModel(R.string.clients, R.drawable.ic_clients, Navigation.Clients), //todo handle clients
-            MenuModel(R.string.settings, R.drawable.ic_build, Navigation.Settings),
-        ),
+    private val menuClient = mutableListOf(
+        MenuModel(R.string.my_products, R.drawable.ic_store, Navigation.Products),
+        MenuModel(R.string.supplies, R.drawable.ic_shopping_cart, Navigation.Supplies),
+        MenuModel(R.string.settings, R.drawable.ic_build, Navigation.Settings),
     )
 
+    private val menuAdmin = menuClient.apply {
+        add(MenuModel(R.string.clients, R.drawable.ic_clients, Navigation.Clients))
+    }
+
+    val menuItems: MutableStateFlow<List<MenuModel>> = MutableStateFlow(emptyList())
+
     init {
+        viewModelScope.launch { setupMenu() }
         setIsLoading()
+    }
+
+    private suspend fun setupMenu() {
+        val result = getUserUseCase()
+
+        if (result is Resource.Success) {
+            val isAdmin = result.data?.isAdmin.orFalse()
+            val items = if(isAdmin) menuAdmin else menuClient
+            menuItems.value = items
+        }
     }
 
     private fun setIsLoading() = viewModelScope.launch {
