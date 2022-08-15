74873931b5465dae92e2459dfd38a694a711c1fc
jvsena42 <jvsena16@gmail.com>
2022-08-15 07:27:30 -0300

    feat: onAction

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/menu/MenuViewModel.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/menu/MenuViewModel.kt
@@ -3,11 +3,14 @@ package com.bulletapps.candypricer.presentation.ui.scenes.main.menu
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bulletapps.candypricer.config.Resource
+import com.bulletapps.candypricer.domain.model.MenuModel
 import com.bulletapps.candypricer.domain.usecase.user.GetUserUseCase
 import com.bulletapps.candypricer.domain.usecase.user.IsExpiredUserUseCase
+import com.bulletapps.candypricer.presentation.ui.scenes.main.user.login.LoginViewModel
 import com.bulletapps.candypricer.presentation.util.EventFlow
 import com.bulletapps.candypricer.presentation.util.EventFlowImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,7 +19,10 @@ class MenuViewModel @Inject constructor(
     private val isExpiredUserUseCase: IsExpiredUserUseCase
 ) : ViewModel(), EventFlow<MenuViewModel.ScreenEvent> by EventFlowImpl()  {
 
-    suspend fun setup() {
+    val uiState = UIState()
+
+    suspend fun setup(menuItems: List<MenuModel>) {
+        uiState.menuItems.value = menuItems
         val result = getUserUseCase()
 
         if (result is Resource.Success) {
@@ -28,8 +34,21 @@ class MenuViewModel @Inject constructor(
         }
     }
 
+    fun onAction(action: ScreenActions) = when(action) {
+        is ScreenActions.OnClickItem -> viewModelScope.sendEvent(ScreenEvent.Navigate(action.path))
+    }
+
+    sealed class ScreenActions {
+        data class OnClickItem(val path: String) : ScreenActions()
+    }
+
     sealed class ScreenEvent {
         object ExpiredScreen : ScreenEvent()
+        data class Navigate(val path: String) : ScreenEvent()
+    }
+
+    class UIState {
+        val menuItems = MutableStateFlow<List<MenuModel>>(emptyList())
     }
 }
 
