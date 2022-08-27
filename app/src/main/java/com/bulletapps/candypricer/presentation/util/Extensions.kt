2b6ee00705288dcf6263cd7c8adc5270172f5e49
jvsena42 <jvsena16@gmail.com>
2022-08-27 12:13:00 -0300

    refactor: message

--- app/src/main/java/com/bulletapps/candypricer/presentation/util/Extensions.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/util/Extensions.kt
@@ -25,27 +25,6 @@ fun Float?.orZero() = this ?: ZERO_FLOAT
 
 fun Boolean?.orFalse() = this ?: false
 
-/*fun Context.openWhatsapp(phone: String = WHATSAPP_NUMBER, message: String = "Oi!") {
-    try {
-        val sendIntent = Intent().apply {
-            action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, message)
-            putExtra("jid", "${phone}@s.whatsapp.net")
-            type = "text/plain"
-            setPackage("com.whatsapp")
-        }
-        startActivity(sendIntent)
-    }catch (e: Exception){
-        e.printStackTrace()
-        val appPackageName = "com.whatsapp"
-        try {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")))
-        } catch (e :android.content.ActivityNotFoundException) {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")))
-        }
-    }
-}*/
-
 fun Context.openWhatsapp(phone: String = WHATSAPP_NUMBER, message: String = "Oi!") {
     val url = "https://api.whatsapp.com/send?phone=$phone"+"&text=" + URLEncoder.encode(message, "UTF-8")
     Intent(Intent.ACTION_VIEW).apply {
