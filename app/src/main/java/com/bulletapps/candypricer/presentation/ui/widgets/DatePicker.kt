1b3151136e54754ee14d41abb30c09bac9e680b6
jvsena42 <jvsena16@gmail.com>
2022-09-06 07:37:08 -0300

    feat: DatePicker wip

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/widgets/DatePicker.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/widgets/DatePicker.kt
@@ -1,6 +1,9 @@
 package com.bulletapps.candypricer.presentation.ui.widgets
 
-import android.widget.DatePicker
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class DatePicker {
     companion object {
@@ -10,6 +13,53 @@ class DatePicker {
         const val DAY_MONTH_YEAR_FORMAT = "dd/MM/yyy"
     }
 
+    var builder = DatePickerBuilder()
+
+    fun show(fragmentManager: FragmentManager) = MaterialDatePicker.Builder.datePicker()
+        .apply {
+            setTitleText(builder.title)
+            builder.validDateRange?.let {
+                val constraints = buildCalendarConstraints(it.first, it.second)
+                setCalendarConstraints(constraints)
+            }
+            builder.validOnlyDateAfter?.let {
+                val constraints = buildCalendarConstraints(it)
+                setCalendarConstraints(constraints)
+            }
+            builder.validDateUntil?.let {
+                val constraints = buildCalendarConstraints(it)
+                setCalendarConstraints(constraints)
+            }
+            builder.selectedDate?.let { setSelection(it) }
+        }
+        .build()
+        .apply {
+            addOnPositiveButtonClickListener { builder.onDateSelect(buildResult(it)) }
+            addOnCancelListener { builder.onCancel() }
+            addOnDismissListener { builder.onDismiss() }
+        }
+        .show(fragmentManager, null)
+
+
+    private fun buildCalendarConstraints(start: Long, end: Long): CalendarConstraints {
+        val startWithoutTime = removeTimeFromDate(start)
+        val endWithoutTime = if (end < start) startWithoutTime else removeTimeFromDate(end)
+
+        return CalendarConstraints.Builder()
+            .setStart(startWithoutTime)
+            .setEnd(endWithoutTime)
+            .setValidator(BetweenDateValidator(startWithoutTime, endWithoutTime))
+            .build()
+    }
+
+    private fun buildCalendarConstraints(start: Long): CalendarConstraints {
+        return CalendarConstraints.Builder()
+            .setValidator(DateValidatorPointForward(removeTimeFromDate(start)))
+            .build()
+    }
+
+
+
     data class DatePickerBuilder(
         var title: String = "",
         var presentationDateFormatResult: String = DAY_MONTH_YEAR_FORMAT,
@@ -18,7 +68,7 @@ class DatePicker {
         var validDateRange: Pair<Long, Long>? = null,
         var validOnlyDateAfter: Long? = null,
         var validDateUntil: Long? = null,
-//        var onDateSelect: (DatePicker.Result) -> Unit = {},
+        var onDateSelect: (DatePicker.Result) -> Unit = {},
         var onCancel: () -> Unit = {},
         var onDismiss: () -> Unit = {}
     )
