3e25406c290bbb38e6f2b3badedc8b32a2842122
jvsena42 <jvsena16@gmail.com>
2022-10-16 09:50:40 -0300

    refactor: remove preferences data store

--- app/src/main/java/com/bulletapps/candypricer/config/di/SupplyCandyPricerDataSource.kt
+++ app/src/main/java/com/bulletapps/candypricer/config/di/SupplyCandyPricerDataSource.kt
@@ -1,13 +1,6 @@
 package com.bulletapps.candypricer.config.di
 
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.datastore.core.DataStore
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.preferencesDataStoreFile
 import com.bulletapps.candypricer.data.api.CandyPricerApi
 import com.bulletapps.candypricer.data.datasource.CandyPricerDataSource
 import com.bulletapps.candypricer.data.datasource.CandyPricerRemoteDataSource
@@ -17,9 +10,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 private const val USER_PREFERENCES = "user_preferences"
