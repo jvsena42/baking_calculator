728b14ada6414c2f56b391f9a207e7c248481f49
jvsena42 <jvsena16@gmail.com>
2022-10-27 07:42:20 -0300

    feat: unputs error messages

---
+++ app/src/main/java/com/bulletapps/candypricer/domain/usecase/inputValidation/ValidateNameUseCase.kt
@@ -0,0 +1,20 @@
+package com.bulletapps.candypricer.domain.usecase.inputValidation
+
+import com.bulletapps.candypricer.R
+import com.bulletapps.candypricer.config.Resource
+import com.bulletapps.candypricer.config.UiText
+import javax.inject.Inject
+
+class ValidateNameUseCase @Inject constructor() : ValidateEmptyTextUseCase {
+    override suspend fun invoke(text: String): Resource<Unit> {
+        return if (text.isNotEmpty()) {
+            Resource.Success(Unit)
+        } else {
+            Resource.Error(
+                UiText.StringResource(
+                    R.string.error_name
+                )
+            )
+        }
+    }
+}
--- app/src/main/java/com/bulletapps/candypricer/domain/usecase/user/CreateUserUseCase.kt
+++ app/src/main/java/com/bulletapps/candypricer/domain/usecase/user/CreateUserUseCase.kt
@@ -4,12 +4,12 @@ import com.bulletapps.candypricer.config.Resource
 import com.bulletapps.candypricer.data.parameters.CreateUserParameters
 import com.bulletapps.candypricer.data.repository.CandyPricerRepository
 import com.bulletapps.candypricer.data.response.LoginResponse
-import com.bulletapps.candypricer.presentation.util.filterNumbers
+import com.bulletapps.candypricer.presentation.util.removeNumbers
 import javax.inject.Inject
 
 class CreateUserUseCase @Inject constructor(private val repository: CandyPricerRepository) {
     suspend operator fun invoke(parameters: CreateUserParameters): Resource<LoginResponse> {
-        val cleanPhone = parameters.copy(phone = parameters.phone.filterNumbers())
+        val cleanPhone = parameters.copy(phone = parameters.phone.removeNumbers())
         return repository.createUser(cleanPhone)
     }
 }
