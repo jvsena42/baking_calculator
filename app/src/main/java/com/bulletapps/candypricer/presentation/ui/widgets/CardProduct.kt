4755e71c85154dbd8915f049df94d9d22baba840
João Victor Sena <jvsena16@gmail.com>
2022-10-30 10:00:51 -0300

    Feat/display unit product detail (#119)
    * feat: create component
    
    * feat: format unit
    
    * fix: add spacer

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/widgets/CardProduct.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/widgets/CardProduct.kt
@@ -3,12 +3,14 @@ package com.bulletapps.candypricer.presentation.ui.widgets
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bulletapps.candypricer.R
+import com.bulletapps.candypricer.presentation.util.EMPTY_STRING
 
 @Composable
 fun CardTwoItemsVertical(
@@ -56,6 +58,34 @@ fun CardTwoItemsHorizontal(
         ) {
             TextWithLabel(stringResource(id = firstLabel), firsName)
             TextWithLabel(stringResource(id = secondLabel), secondName)
+        }
+    }
+}
+@Composable
+fun CardTwoItemsWithDetailHorizontal(
+    firstLabel: Int,
+    firsName: String,
+    secondLabel: Int,
+    secondName: String,
+    thirdName: String = EMPTY_STRING,
+    onClick: () -> Unit?
+) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .clickable { onClick.invoke() }
+            .fillMaxWidth(),
+        elevation = 1.dp
+    ) {
+        Row(
+            modifier = Modifier.padding(12.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextWithLabel(stringResource(id = firstLabel), firsName)
+            Spacer(modifier = Modifier.weight(1f))
+            TextWithLabel(stringResource(id = secondLabel), secondName)
+            Spacer(modifier = Modifier.width(2.dp))
+            Text(thirdName)
         }
     }
 }
