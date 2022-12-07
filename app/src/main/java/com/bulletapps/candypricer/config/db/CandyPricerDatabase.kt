c674225285da3120fdf01a05740504ac19ba0c92
jvsena42 <jvsena16@gmail.com>
2022-12-07 07:53:07 -0300

    fix: increase database version

--- app/src/main/java/com/bulletapps/candypricer/config/db/CandyPricerDatabase.kt
+++ app/src/main/java/com/bulletapps/candypricer/config/db/CandyPricerDatabase.kt
@@ -7,7 +7,7 @@ import com.bulletapps.candypricer.data.entities.UserEntity
 
 @Database(
     entities = [UnitEntity::class, UserEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class CandyPricerDatabase : RoomDatabase() {
