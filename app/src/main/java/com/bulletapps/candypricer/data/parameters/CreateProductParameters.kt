597a26b609af78453f4155ce001765455a7e2bb2
jvsena42 <jvsena16@gmail.com>
2023-03-12 11:11:10 -0300

    fix: create product

--- app/src/main/java/com/bulletapps/candypricer/data/parameters/CreateProductParameters.kt
+++ app/src/main/java/com/bulletapps/candypricer/data/parameters/CreateProductParameters.kt
@@ -5,18 +5,16 @@ import com.google.gson.annotations.SerializedName
 data class CreateProductParameters(
     @SerializedName("name")
     var name: String,
-    @SerializedName("unit_id")
-    var unitId: Int,
     @SerializedName("quantity")
     var quantity: Double,
-    @SerializedName("supplies")
-    var suppliesId: List<Int>,
+    @SerializedName("supply_amount")
+    val amountQuantitySupply: List<SupplyAmountParameters>,
     @SerializedName("profit_margin")
     var profitMargin: Double,
     @SerializedName("labor_work")
     var laborValue: Double,
     @SerializedName("variable_expenses")
     var variableExpenses: Double,
-    @SerializedName("amount_quantity_supply")
-    val amountQuantitySupply: List<Double>,
+    @SerializedName("unit_id")
+    var unitId: Int,
 )
