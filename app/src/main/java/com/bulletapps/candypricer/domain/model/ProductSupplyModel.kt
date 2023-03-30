276054aab085de1414ed367cb3794fd268fcef26
jvsena42 <jvsena16@gmail.com>
2023-03-30 07:53:57 -0300

    fix: unit and product supply mapper

--- app/src/main/java/com/bulletapps/candypricer/domain/model/ProductSupplyModel.kt
+++ app/src/main/java/com/bulletapps/candypricer/domain/model/ProductSupplyModel.kt
@@ -2,6 +2,5 @@ package com.bulletapps.candypricer.domain.model
 
 data class ProductSupplyModel(
     val quantity: Double,
-    var supplyId: Int,
-    var name: String,
+    var supply: SupplyModel
 )
