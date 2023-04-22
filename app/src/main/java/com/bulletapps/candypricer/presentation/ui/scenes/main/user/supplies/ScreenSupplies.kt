c95eafa1beb8f7e7c79c73d64d7737285634209b
jvsena42 <jvsena16@gmail.com>
2023-04-22 10:37:06 -0300

    fix: scaffold parameter

--- app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/user/supplies/ScreenSupplies.kt
+++ app/src/main/java/com/bulletapps/candypricer/presentation/ui/scenes/main/user/supplies/ScreenSupplies.kt
@@ -1,10 +1,16 @@
 package com.bulletapps.candypricer.presentation.ui.scenes.main.user.supplies
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -105,8 +111,10 @@ private fun ScreenContent(
                 )
             }
         }
-    ) {
-        MakeList(uiState, onAction)
+    ) { contentPadding ->
+        Box(modifier = Modifier.padding(contentPadding)) {
+            MakeList(uiState, onAction)
+        }
     }
 }
 
